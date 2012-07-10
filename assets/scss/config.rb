6c038faeb84ed6a2ded4f524a4f6f5a01a962531
Marco Solazzi <marco.solazzi@gmail.com>
2012-07-10 18:07:13 +0200

    compass files and compile config

---
+++ assets/scss/config.rb
@@ -0,0 +1,10 @@
+environment = :development
+#environment = :production
+
+sass_dir = "."
+css_path = "../touchTouch"
+images_dir = "../touchTouch"
+http_images_path = "./"
+line_comments = false
+
+Sass::Script::Number.precision = 10
